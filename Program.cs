ac37c6d6f3a5621edfc6fcd98fe52d60de018313
Albert Sevy <albertstorres@gmail.com>
2024-01-23 16:02:35 -0300

    Finalizando o repositório da aula de Guids.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Guids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid(); //Criando novo Guid
+
+            id.ToString();//Convertendo tipo Guid para string
+
+            id = new Guid("57ff4b1c-849d-4ef4-ac7b-4da6700cf53e");//Inserindo um Guid específico a uma variável
+
+            Console.WriteLine(id);
+        }
+    }
+}
